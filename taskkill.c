be947386a2e5cdd0794736aa1fd51b9da00c5642
Ciro Meneses <ciroccuva@hotmail.com>
2015-11-19 01:39:57 -0300

    input treatment for dementia people

--- taskkill.c
+++ taskkill.c
@@ -9,7 +9,7 @@ void mata_processo(char *comando);
 
 FILE *arq;
 int op, i;
-char processo[1000], cmd_kill[2000];
+char processo[1000], cmd_kill[2000], op_menu[2000];
 
 int main() {
 
@@ -32,13 +32,29 @@ void lista_processos() {
 }
 
 int menu() {
-  printf("\n##  1 - List Process");
-  printf("\n##  2 - Kill Process by PID");
-  printf("\n##  3 - Kill Process by name");
-  printf("\n##  0 - EXIT");
+ int valida = 1;
+
+  printf("\n##  1   | List Process");
+  printf("\n##  2   | Kill Process by PID");
+  printf("\n##  3   | Kill Process by name");
+  printf("\n##  4+  | Clear screen");
+  printf("\n##  0   | EXIT");
   printf("\n\n##  Your Option is: ");
 
-  scanf("%d", &op);
+  do {
+    valida = 1;
+    scanf("%s", op_menu);
+
+    for (i = 0; op_menu[i] != '\0'; i++)
+      if( isdigit(op_menu[i]) == 0 )
+        valida = 0;
+
+    if (valida == 0)
+      printf("\nOnly positive, numbers: ");
+
+  } while (valida != 1);
+
+  op = atoi(op_menu);
 
   if (op == 0)
     return 0;
@@ -48,6 +64,8 @@ int menu() {
     mata_processo_PID();
   else if (op == 3)
     mata_processo_nome();
+  else
+    system("cls");
 
   return 1;
 }
@@ -57,7 +75,7 @@ void mata_processo(char *comando) {
 }
 
 void mata_processo_nome() {
-  printf("\nName of process(example: \"chrome.exe\"):");
+  printf("\nName of process(example: \"chrome.exe\"): ");
   scanf("%s", processo);
 
   sprintf(cmd_kill, "TASKKILL /F /IM \"%s\"", processo);
@@ -67,7 +85,7 @@ void mata_processo_nome() {
 }
 
 void mata_processo_PID() {
-  printf("\nPID of process(example: \"1050\"):");
+  printf("\nPID of process(example: \"1050\"): ");
   scanf("%s", processo);
 
   sprintf(cmd_kill, "TASKKILL /F /PID \"%s\"", processo);
